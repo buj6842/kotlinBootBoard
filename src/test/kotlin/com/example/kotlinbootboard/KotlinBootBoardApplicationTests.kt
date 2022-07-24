72f565ec50904663329fb4b8f147e551efe6fd28
변의진 <parrot@byeon-uijin-ui-MacBookPro.local>
2022-07-24 11:46:15 +0900

    프로젝트 초기 구성

---
+++ src/test/kotlin/com/example/kotlinbootboard/KotlinBootBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinbootboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBootBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
